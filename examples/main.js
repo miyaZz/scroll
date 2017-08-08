f8b9c8410f3fe28f0941def9df7810fc67f3994e
123 <miya@miyadeMacBook-Pro.local>
2017-08-09 00:46:14 +0800

    添加墓碑占位符

--- examples/main.js
+++ examples/main.js
@@ -4,12 +4,86 @@ import Scroll from '../src/Scroll';
 import Infinite from '../src/Infinite';
 import './style.less';
 
-class Main extends Component {
-  render() {
-    let init = fetch(100);
+// 生成0～1000的随机数
+function randomFn() {
+  return _.random(0, 1000);
+}
 
-    return <Infinite className='center'/>;
-      {/* <Scroll className='left' length={1000}/> */}
+// 模拟异步加载函数 每次默认加载20条数据
+function fetch(count, size) {
+  let number = 20;
+  let list = [];
+  let _has = 0;
+  if(size) number = size;
+  if(count) _has = count;
+  for (let i = _has; i < _has + size; i++) {
+    list.push({id: i, content: `第${i}条`});
+  }
+  return list;
+}
+
+class Main extends Component {
+
+  constructor(props) {
+    super(props);
+    this.pageSize = 20;
+    this.state = {
+      length: 30,
+      dataSource: fetch(null, 30)
+    }
+  }
+
+  scroll = () => {
+    const {length, dataSource} = this.state;
+    const _self = this;
+    setTimeout(() => {
+      _self.setState({
+        length: length + 20,
+        dataSource: dataSource.concat(fetch(length, this.pageSize))
+      });
+    }, 3000);
+  }
+
+  renderItem = o => {
+    return (
+      <div  className={o.id%2 ? 'item' : 'item-trip'} key={o.id} id={o.id}>
+      <div>我是第{o.id}个元素～</div>
+      {o.id%2 !== 0 && <div>奇数我就变白了😊</div>}
+      {o.id%2 === 0 && <div>我是沙漠中的第{o.id}片绿洲🙃️</div>}
+    </div>
+    )
+  };
+
+  tombstoneRender = o => {
+    return(
+      <div className={o.id%2 ? 'item' : 'item-trip'} key={o.id} id={o.id}>
+        <div>狂拽酷炫脑白金，我就是职业占地盘的</div>
+        <div>收费标准：占一个位100块～</div>
+        <div>你咋滴，不服，不服我就走😳</div>
+      </div>
+    )
+  }
+
+  render() {
+    return( 
+    <div>
+      {/* <Infinite
+        className='left'
+        itemRenderer={this.renderItem}
+        dataSource={fetch(null, 2000)}
+        async={false}
+      /> */}
+      <Infinite
+        className='right'
+        itemRenderer={this.renderItem}
+        tombstoneRender={this.tombstoneRender}
+        async={true}
+        dataSource={this.state.dataSource}
+        pageSize={this.pageSize}
+        scroll={this.scroll}
+      />    
+    </div>
+    );
   }
 }
 
